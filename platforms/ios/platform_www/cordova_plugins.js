6399a9bc3f8d3a5d312f4eee11b432c9a7394e64
ypandoo <ypandoo@126.com>
2017-06-05 11:24:15 +0800

    plugin init branch

--- platforms/ios/platform_www/cordova_plugins.js
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -48,6 +48,14 @@ module.exports = [
             "cordova.plugins.Keyboard"
         ],
         "runs": true
+    },
+    {
+        "id": "cordova-plugin-jdwear.jdwear",
+        "file": "plugins/cordova-plugin-jdwear/www/jdwear.js",
+        "pluginId": "cordova-plugin-jdwear",
+        "clobbers": [
+            "cordova.plugins.jdwear"
+        ]
     }
 ];
 module.exports.metadata = 
@@ -58,7 +66,8 @@ module.exports.metadata =
     "cordova-plugin-splashscreen": "4.0.3",
     "cordova-plugin-statusbar": "2.2.2",
     "cordova-plugin-whitelist": "1.3.1",
-    "ionic-plugin-keyboard": "2.2.1"
+    "ionic-plugin-keyboard": "2.2.1",
+    "cordova-plugin-jdwear": "0.0.1"
 };
 // BOTTOM OF METADATA
 });
--- src/pages/self/self.ts
+++ src/pages/self/self.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { SetActivityPage } from '../set-activity/set-activity'
+import { Platform } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
+
+declare var cordova: any;
 
 @Component({
   selector: 'page-self',
@@ -8,14 +12,24 @@ import { SetActivityPage } from '../set-activity/set-activity'
 })
 export class SelfPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public platform: Platform, public toastCtrl: ToastController) {
 
   }
 
-  pushPage(){
-    this.navCtrl.push(SetActivityPage);
+  pushPage() {
+    //this.navCtrl.push(SetActivityPage);
+    this.platform.ready().then(() => {
+      cordova.plugins.jdwear.coolMethod(
+        "======> WORK"
+        ,(e)=>{
+          let toast = this.toastCtrl.create({
+                        message: e,
+                        duration: 3000
+                      });
+          toast.present();
+        }
+        ,(e)=>{console.log(e)}
+        );
+    });
   }
-
-  
-
 }
